14bc579d6989b1fc75be7bbeb40d0451b6e15a59
hellobpm <hellobpm@gmail.com>
2018-11-30 12:51:35 +0900

    aa

--- src/main/java/jj/spring/boot/demo/AAA.java
+++ src/main/java/jj/spring/boot/demo/AAA.java
@@ -1,8 +1,11 @@
 package jj.spring.boot.demo;
 
+import java.util.stream.Stream;
+
 public class AAA {
 
 	public static void main(String[] args) {
 		System.out.println("AAA.main");
+		Stream.of(1, 2, 3).forEach(System.out::println);
 	}
 }
